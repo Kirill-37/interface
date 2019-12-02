e4378959b380e759029d3798d04bdb9b7cf22f2d
Kirill Kharuzin <k.kharuzin@gmail.com>
2019-12-03 00:51:42 +0300

    дз 2

--- Client for VK/Client for VK/LoginViewController.swift
+++ Client for VK/Client for VK/LoginViewController.swift
@@ -8,35 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet weak var signInLogin: UITextField!
-    @IBOutlet weak var signInPassword: UITextField!
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var password: UITextField!
     
     private let rightLogin = "Kharuzin"
     private let rightPassword = "123456"
-    
+   
     @IBOutlet weak var logoVK: UIImageView!
+    
+    @IBAction func loginButton (_ sender: UIButton) {
+        hideKeyboard()
+        guard
+            let loginInput = login.text, !loginInput.isEmpty,
+            let passwordInput = password.text, !passwordInput.isEmpty else {
+                showAlert(title: "Ошибка", message: "Пожалуйста, введите данные", titleAction: "Ok")
+                password.text = ""
+                return
+        }
+        if loginInput == rightLogin && passwordInput == rightPassword {
+            print(loginInput + " " + passwordInput)
+        } else {
+            showAlert(title: "Ошибка", message: "Вы ввели не корректные данные", titleAction: "Ok")
+            login.text = ""
+            password.text = ""
+        }
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         let hideAction = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
         view.addGestureRecognizer(hideAction)
+        
+        login.delegate = self
+        password.delegate = self
     }
     
     @objc func hideKeyboard() {
         view.endEditing(true)
     }
     
-    @IBAction func loginButton(_ sender: Any) {
-        guard let loginInput = signInLogin.text, !loginInput.isEmpty,
-            let passwordInput = signInPassword.text, !passwordInput.isEmpty else {
-                showAllert(title: "Error!", message: "Login or Password was not entered", titleAction: "OK")
+    func showAlert(title: String, message: String, titleAction: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: titleAction, style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
         }
-        print(loginInput + " " + passwordInput)
-    }
     
-
+    func textFieldShouldReturn (_ textField: UITextField) -> Bool {
+        if let nextField = textField.superview?.viewWithTag(textField.tag + 1) as? UITextField {
+            nextField.becomeFirstResponder()
+        } else {
+            textField.resignFirstResponder()
+            return true;
+        }
+        return false
+    }
 }
-
